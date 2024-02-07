34de064a51a656ac1ae162c11460f9639cfb0b75
João Marcos <joaombcerqueira@gmail.com>
2024-02-06 23:47:01 -0300

    refactor: moves handler to internal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/joaomarcosbc/api-fc/configs"
-	"github.com/joaomarcosbc/api-fc/internal/dto"
 	"github.com/joaomarcosbc/api-fc/internal/entity"
 	"github.com/joaomarcosbc/api-fc/internal/infra/database"
+	"github.com/joaomarcosbc/api-fc/internal/webservice/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,41 +24,9 @@ func main() {
 	db.AutoMigrate(&entity.Product{})
 
 	productDB := database.NewProduct(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
 	http.HandleFunc("/products", productHandler.CreateProduct)
 
 	http.ListenAndServe(":8000", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if err = h.ProductDB.Create(p); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
 }
